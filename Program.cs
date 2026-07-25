a2f8834919726c558b8687fcd07361af21136301
Shoontaro <tishinadw@gmail.com>
2026-07-26 02:16:41 +0300

    try to change api method

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel;
-using Spectre.Console.Cli;
+﻿using Octokit;
 using Spectre.Console;
+using Spectre.Console.Cli;
 using System.CommandLine;
-using System.Net.Http.Headers;
 using System.CommandLine.Parsing;
+using System.ComponentModel;
+using System.Net.Http.Headers;
 
 internal class Program
 {
@@ -20,23 +21,59 @@ internal class Program
 
         rootCommand.SetAction(parseResult =>
         {
+
+            string username = parseResult.GetValue(name);
+
             Console.WriteLine($"Name: {parseResult.GetValue(name)}");
+            try
+            {
+                Console.WriteLine($"Запрос активности для пользователя: {username}...");
+                GetUserActivityAsync(username);
+            }
+            catch (ApiException ex)
+            {
+                Console.WriteLine($"Ошибка API GitHub: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
         });
 
         rootCommand.Parse(args).Invoke();
     }
 
-    private static readonly HttpClient client = new HttpClient();
+    private static readonly GitHubClient client = new GitHubClient( new Octokit.ProductHeaderValue("GithubUserActivity"));
+    
 
-    private void gitApi()
+    static async Task GetUserActivityAsync(string username)
     {
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
-        client.DefaultRequestHeaders.Add("User-Agent", "CSharp-Console-GitHub-App");
+        // client.Credentials = new Credentials("ваш_personal_access_token");
+        // Получаем список публичных событий пользователя (ограничено последними 300 событиями)
+        var events = await client.Activity.Events.GetAllUserPerformedPublic(username);
 
+        if (events.Count == 0)
+        {
+            Console.WriteLine("Публичная активность за последние 90 дней не найдена.");
+            return;
+        }
 
+        Console.WriteLine($"\nНайдено записей об активности: {events.Count}");
 
+        // Извлекаем самое последнее событие
+        var lastEvent = events[0];
+        Console.WriteLine($"\nПоследнее действие:");
+        Console.WriteLine($"- Тип события: {lastEvent.Type}");
+        Console.WriteLine($"- Репозиторий: {lastEvent.Repo.Name}");
+        Console.WriteLine($"- Дата и время: {lastEvent.CreatedAt.ToLocalTime()}");
     }
+
+    //private void gitApi()
+    //{
+    //    client.DefaultRequestHeaders.Accept.Clear();
+    //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+    //    client.DefaultRequestHeaders.Add("User-Agent", "CSharp-Console-GitHub-App");
+    //}
 
     public void Commands()
     {
